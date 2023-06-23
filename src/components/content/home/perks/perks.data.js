d30fbb4d66f260b495f401e237a01f6614b507ac
AdamAngg <adam.nglart@gmail.com>
2023-06-23 12:52:37 +0200

    chore: move text from perk to perk.data

--- src/components/content/home/perks/perks.data.js
+++ src/components/content/home/perks/perks.data.js
@@ -3,9 +3,29 @@ import concretemix from "../../../../../public/assets/img/perks/concrete-mixer.p
 import project from "../../../../../public/assets/img/perks/project-management.png";
 import trophy from "../../../../../public/assets/img/perks/trophy.png";
 
-export const iconsArray = [
-  [cement, "Zdjecie przedstawia ikonę worka cementu"],
-  [concretemix, "Zdjecie przedstawia ikonę maszyny do mieszania betonu"],
-  [project, "Zdjecię przedstawią ikonę projektu"],
-  [trophy, "Zdjęcie przedstawia ikonę puchary"],
+export const perksArray = [
+  {
+    icon: cement,
+    altText: "Zdjecie przedstawia ikonę worka cementu",
+    title: "Lorem Ipsum",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, sunt,nemo numquam, sapiente nesciunt nihil a maxime quia totam neque aliasquibusdam culpa accusantium doloremque veritatis unde est molestiaeenim harum animi error. Molestias exercitationem sit iure pariaturnulla fugit facere est facilis saepe corporis quam voluptatem, non, rem voluptatum!",
+  },
+  {
+    icon: concretemix,
+    altText: "Zdjecie przedstawia ikonę maszyny do mieszania betonu",
+    title: "Lorem Ipsum",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, sunt,nemo numquam, sapiente nesciunt nihil a maxime quia totam neque aliasquibusdam culpa accusantium doloremque veritatis unde est molestiaeenim harum animi error. Molestias exercitationem sit iure pariaturnulla fugit facere est facilis saepe corporis quam voluptatem, non, rem voluptatum!",
+  },
+  {
+    icon: project,
+    altText: "Zdjecię przedstawią ikonę projektu",
+    title: "Lorem Ipsum",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, sunt,nemo numquam, sapiente nesciunt nihil a maxime quia totam neque aliasquibusdam culpa accusantium doloremque veritatis unde est molestiaeenim harum animi error. Molestias exercitationem sit iure pariaturnulla fugit facere est facilis saepe corporis quam voluptatem, non, rem voluptatum!",
+  },
+  {
+    icon: trophy,
+    altText: "Zdjęcie przedstawia ikonę puchary",
+    title: "Lorem Ipsum",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, sunt,nemo numquam, sapiente nesciunt nihil a maxime quia totam neque aliasquibusdam culpa accusantium doloremque veritatis unde est molestiaeenim harum animi error. Molestias exercitationem sit iure pariaturnulla fugit facere est facilis saepe corporis quam voluptatem, non, rem voluptatum!",
+  },
 ];
--- src/components/content/home/perks/perks.jsx
+++ src/components/content/home/perks/perks.jsx
@@ -1,30 +1,23 @@
 import styles from "./perks.module.css";
-import { iconsArray } from "./perks.data";
+import { perksArray } from "./perks.data";
 import Image from "next/image";
 
 export const Perks = () => {
-  const icons = iconsArray.map(([icons, altText], i) => (
+  const perks = perksArray.map((perk, i) => (
     <>
       <div key={i} className={styles.column_container}>
         <div className={styles.border}></div>
         <Image
-          src={icons}
+          src={perk.icon}
           className={styles.icons}
           priority={true}
-          alt={altText}
+          alt={perk.altText}
           blurDataURL="../../../../../public/assets/img/header/blur.jpg"
         />
-        <span className={styles.title}>Lorem Ipsum</span>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, sunt,
-          nemo numquam, sapiente nesciunt nihil a maxime quia totam neque alias
-          quibusdam culpa accusantium doloremque veritatis unde est molestiae
-          enim harum animi error. Molestias exercitationem sit iure pariatur
-          nulla fugit facere est facilis saepe corporis quam voluptatem, non,
-          rem voluptatum!
-        </p>
+        <span className={styles.title}>{perk.title}</span>
+        <p className={styles.text}>{perk.text}</p>
       </div>
     </>
   ));
-  return <div className={styles.container}>{icons}</div>;
+  return <div className={styles.container}>{perks}</div>;
 };
