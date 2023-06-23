72b34b21f6d0a7db022fc0609ab4a0e46c81acd6
AdamAngg <adam.nglart@gmail.com>
2023-06-23 15:06:01 +0200

    feat: perks icons slow load

--- src/components/content/home/perks/perks.jsx
+++ src/components/content/home/perks/perks.jsx
@@ -1,15 +1,18 @@
 import styles from "./perks.module.css";
 import { perksArray } from "./perks.data";
 import Image from "next/image";
+import { UseSlowLoad } from "@/hooks/useSlowLoad.hook";
 
 export const Perks = () => {
+  const { imageLoaded, handleImageLoad } = UseSlowLoad();
   const perks = perksArray.map((perk, i) => (
     <>
       <div key={i} className={styles.column_container}>
         <div className={styles.border}></div>
         <Image
+          onLoad={handleImageLoad}
           src={perk.icon}
-          className={styles.icons}
+          className={`${styles.icons} ${imageLoaded ? styles.loaded : ""}`}
           priority={true}
           alt={perk.altText}
           blurDataURL="../../../../../public/assets/img/header/blur.jpg"
