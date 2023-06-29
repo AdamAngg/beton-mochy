f84813e720380763bb3bab70aaae80774f911053
AdamAngg <adam.nglart@gmail.com>
2023-06-29 17:35:23 +0200

    feat:working slider

--- src/components/content/home/product/product.jsx
+++ src/components/content/home/product/product.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from "react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import styles from "./product.module.css";
-import transitionStyles from "./productTransition.module.css";
+import { useState } from "react";
 
+import styles from "./product.module.css";
+
+import { useProductSlider } from "./useProductSlider.hook";
 import zd1 from "@/../public/assets/img/product/1.png";
 import zd2 from "@/../public/assets/img/product/2.png";
 import zd3 from "@/../public/assets/img/product/3.png";
@@ -13,54 +13,34 @@ import zd7 from "@/../public/assets/img/product/7.png";
 
 export const Product = () => {
   const [items, setItems] = useState([zd1, zd2, zd3, zd4, zd5, zd6, zd7]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [translateX, setTranslateX] = useState(0);
-  const ref = useRef();
-
-  const handleClickPrevious = () => {
-    setTranslateX((prevTranslateX) => prevTranslateX + 100);
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? items.length - 1 : prevIndex - 1
-    );
-    updateItems();
-  };
-
-  const handleClickNext = () => {
-    setTranslateX((prevTranslateX) => prevTranslateX - 100);
-    setCurrentIndex((prevIndex) => (prevIndex === 7 ? 1 : prevIndex + 1));
-    console.log(currentIndex);
-
-    if (currentIndex === 6) {
-      setItems([zd1, zd2, zd3, zd4, zd5, zd6, zd7]);
-      setTranslateX(0);
-    } else updateItems();
-  };
-  const style = {
-    transition: currentIndex === 7 ? " " : "all 0.3s !important ",
-    transition: currentIndex !== 7 ? "all 0.3s !important " : " ",
-    transform: `translateX(${translateX}%)`,
-  };
-
-  const updateItems = () => {
-    const zdjecie = items[currentIndex === 7 ? 0 : currentIndex];
-    setItems((prevItems) => [...prevItems, zdjecie]);
-  };
+  const { handleClickPrevious, handleClickNext, currentIndex, style } =
+    useProductSlider({ array: items });
 
   return (
     <div className={styles.slider}>
       <button onClick={handleClickPrevious}>Previous</button>
-      <div className="container">
-        <div className={styles.sliderItems}>
+      <div className={styles.container}>
+        <div className={styles.sliderItems} style={style}>
           {items.map((item, index) => {
             console.log(item, index, currentIndex);
             return (
-              <div
-                ref={ref}
-                key={index}
-                className={styles.sliderItem}
-                style={style}
-              >
+              <div key={`clone2${index}`} className={styles.sliderItem}>
+                <img src={item.src} alt={`Image clone2 ${index + 1}`} />
+              </div>
+            );
+          })}
+          {items.map((item, index) => {
+            return (
+              <div key={index} className={styles.sliderItem}>
                 <img src={item.src} alt={`Image ${index + 1}`} />
+              </div>
+            );
+          })}
+          {items.map((item, index) => {
+            console.log(item, index, currentIndex);
+            return (
+              <div key={`clone${index}`} className={styles.sliderItem}>
+                <img src={item.src} alt={`Image clone ${index + 1}`} />
               </div>
             );
           })}
--- src/components/content/home/product/useProductSlider.hook.js
+++ src/components/content/home/product/useProductSlider.hook.js
@@ -1,1 +1,50 @@
-export const useProductSlider = () => {};
+import { useState } from "react";
+export const useProductSlider = ({ array }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [translateX, setTranslateX] = useState(-140);
+  const [transition, setTransition] = useState("all 0.3s");
+  const [disabled, setDisabled] = useState(false);
+
+  const blockNavigation = () => {
+    setDisabled(true);
+    if (disabled) {
+      return 0;
+    }
+    setTimeout(() => {
+      setDisabled(false);
+    }, 500);
+  };
+
+  const handleClickPrevious = () => {
+    if (!disabled) {
+      setTranslateX((prevTranslateX) => prevTranslateX + 20);
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+    blockNavigation();
+  };
+
+  const handleClickNext = () => {
+    if (!disabled) {
+      setTranslateX((prevTranslateX) => prevTranslateX - 20);
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+    blockNavigation();
+  };
+
+  const style = {
+    transition: `${transition}`,
+    transform: `translateX(${translateX}%)`,
+  };
+  if (currentIndex === array.length || currentIndex === -array.length) {
+    setTimeout(() => {
+      setTransition("none");
+      setTranslateX(-140);
+      setCurrentIndex(0);
+      setTimeout(() => {
+        setTransition("all 0.3s");
+      }, 50);
+    }, 300);
+  }
+
+  return { handleClickPrevious, handleClickNext, currentIndex, style };
+};
